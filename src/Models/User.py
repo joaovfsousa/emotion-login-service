c9fab4ebebe23938efd84476e1da20da27d4008d
João Vitor Sousa <joaovfsousa@gmail.com>
2022-04-10 02:55:37 -0300

    chore: create user model

---
+++ src/Models/User.py
@@ -0,0 +1,13 @@
+from src.Helpers.HashHelper import HashHelper
+
+
+class User:
+    def __init__(self, id, name, username, password):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.id = id
+
+    def checkPassword(self, password: str) -> bool:
+        passwordHash = HashHelper.hash_string(password)
+        return passwordHash == self.password
