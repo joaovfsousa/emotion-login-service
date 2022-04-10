7caa15230cb35ad822d262c404a7cda85180a26e
João Vitor Sousa <joaovfsousa@gmail.com>
2022-04-10 02:56:15 -0300

    feat: return token on login and handle errors

--- handler.py
+++ handler.py
@@ -1,13 +1,28 @@
 import json
+import logging
+from src.HttpException import HttpException
+from src.Services.Auth import AuthService
+from src.Repositories.User import UserRepository
 
 from src.Helpers.Validator import Validator
 from src.Helpers.HttpHelper import HttpHelper
 
 
 def login(event, context):
-    body = json.loads(event["body"])
+    try:
+        body: dict = json.loads(event["body"])
 
-    if not Validator.is_body_valid(body, ["user", "password"]):
-        return HttpHelper.makeError(400, "400-InvalidBody")
+        if not Validator.is_body_valid(body, ["username", "password"]):
+            HttpException("Verifique o corpo da requisição", 400, "400-InvalidBody")
 
-    return {"statusCode": 204}
+        userRepository = UserRepository()
+        authService = AuthService(userRepository)
+
+        token = authService.authenticate(body["username"], body["password"])
+
+        return HttpHelper.makeResponse(200, {"token": token})
+    except HttpException as ex:
+        return HttpHelper.makeError(ex)
+    except Exception as ex:
+        logging.error(ex)
+        return {"statusCode": 500, "body": json.dumps({"message": "Erro do servidor"})}
